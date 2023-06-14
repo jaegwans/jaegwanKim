530561085c53dfd0dc36d7cbfd14fc6388d01fcd
jaegwans <skhu201811140@gmail.com>
2023-06-14 16:07:32 +0900

    feat: 이력서 연결

--- packages/core/constants/General/index.ts
+++ packages/core/constants/General/index.ts
@@ -1,11 +1,10 @@
-import { githubUrl } from '../SocialMedia';
-
 // using both app's footer
 export const authorName = 'jaegwans';
 
 // Write ever you want :D, default is github
 // 직접 작성해주셔도 좋습니다. 기본적으로 깃허브 주소를 사용합니다.
-export const defaultUrl = githubUrl;
+export const defaultUrl =
+  'https://docs.google.com/document/d/1D5oPFkMqpe1dYCPoW6LAHp9gBBHgHQtdWg11ADhVP1A/edit?usp=sharing';
 
 // using both app's favicon
 export * as favicon from './favicon.ico';
