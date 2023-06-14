88b3718982a6e6e732a38e9859e6f57004a6b45c
jaegwans <skhu201811140@gmail.com>
2023-06-14 16:05:28 +0900

    feat: 소셜 미디어

--- packages/core/constants/SocialMedia/index.ts
+++ packages/core/constants/SocialMedia/index.ts
@@ -1,12 +1,12 @@
 // Please do not write '/' at last
 // 마지막에 '/'를 빼주세요!
 
-export const githubUrl = 'https://github.com/hyesungoh';
+export const githubUrl = 'https://github.com/jaegwans';
 
-export const instagramUrl = 'https://www.instagram.com/hyes5_';
+export const instagramUrl = 'https://www.instagram.com/jae_gwans';
 
-export const facebookUrl = 'https://www.facebook.com/hyesungoh414';
+export const facebookUrl = 'https://www.facebook.com/jaegwans';
 
-export const linkedInUrl = 'https://www.linkedin.com/in/hyesungoh414';
+export const linkedInUrl = 'https://www.linkedin.com/in/jaegwans';
 
 export const twitterUrl = '';
